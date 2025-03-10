070f5f9e8fcb0f55712be0f81d421300cd2ed2a1
FionnStack <c22352591@mytudublin.ie>
2025-03-10 14:15:30 +0000

    Updated test to use Pytest fixtures

--- chimerapy/tests/test_chimera.py
+++ chimerapy/tests/test_chimera.py
@@ -6,12 +6,19 @@ from sunpy.map import Map
 from chimerapy.chimera import generate_candidate_mask
 from examples.paper_figures import mask_map
 
-m171 = Map("https://jsoc1.stanford.edu/data/aia/synoptic/2016/10/31/H0200/AIA20161031_0232_0171.fits")
-m193 = Map("https://jsoc1.stanford.edu/data/aia/synoptic/2016/10/31/H0200/AIA20161031_0232_0193.fits")
-m211 = Map("https://jsoc1.stanford.edu/data/aia/synoptic/2016/10/31/H0200/AIA20161031_0232_0211.fits")
+@pytest.fixture(scope="module")
+def m171():
+    return Map("https://jsoc1.stanford.edu/data/aia/synoptic/2016/10/31/H0200/AIA20161031_0232_0171.fits")
 
-def test_generate_candidate_mask():
+@pytest.fixture(scope="module")
+def m193():
+    return Map("https://jsoc1.stanford.edu/data/aia/synoptic/2016/10/31/H0200/AIA20161031_0232_0193.fits")
 
+@pytest.fixture(scope="module")
+def m211():
+    return Map("https://jsoc1.stanford.edu/data/aia/synoptic/2016/10/31/H0200/AIA20161031_0232_0211.fits")
+
+def test_generate_candidate_mask(m171, m193, m211):
     result_mask = generate_candidate_mask(m171, m193, m211)
 
     expected_shape = m171.data.shape
